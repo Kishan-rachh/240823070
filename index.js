2d57f30887f806806b5b3da87618bc9b29769f8c
atmiya <you@example.com>
2025-07-14 11:49:07 +0530

    program

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-const surname="rachh"
-console.log(surname)
+// const surname="rachh"
+// console.log(surname)
 
-const name="Kishan"
-console.log(name)
+// const name="Kishan"
+// console.log(name)
+
+
+//1 
+let num = 7
+if(num % 2==0){
+    console.log("Number is even")
+}else{
+    console.log("Number is odd")
+}
+
+// 2
+const a = 5.9
+const b =9
+if(a>b){
+    console.log("a is max")
+}else{
+    console.log("a is min")
+}
+
+// 3
+const year=2020
+
+if(year % 4 ==0){
+    console.log(year+"year is leap")
+}else{
+    console.log(`${year}year is no leap`)
+}
